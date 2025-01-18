4649a3845f41ca7101b55a77f1750bb2e2a4ae59
omjain4 <119519103+omjain4@users.noreply.github.com>
2025-01-18 22:39:50 +0530

    mongo after commit

---
+++ src/index.js
@@ -0,0 +1,34 @@
+// require("dotenv").config(path: './env');
+
+import dotenv from "dotenv";
+import express from "express";
+import mongoose, { connect } from "mongoose";
+import { DB_NAME } from "./constants";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+});
+
+connectDB()
+
+// import express from "express";
+// const app = express();
+// (async () => {
+//     try {
+//         await mongoose.connect(`${process.env.
+//             MONGODB_URL}/${DB_NAME}`)
+//             app.on("error", (err) => {
+//                 console.log("Error in server setup", err);
+//                throw err;
+//             })
+           
+//             app.listen(process.env.PORT, () => {
+//                console.log(`Server is listeing on port ${process.env.PORT}`);
+//             });
+
+//         }catch (error) {
+//           console.log("Connected to MongoDB", error);
+//           throw err;
+//     }
+// })()
