7d70fa2039c0fb5428ec94eeee150f628d987bb6
omjain4 <119519103+omjain4@users.noreply.github.com>
2025-01-19 23:21:47 +0530

    model-erroprfree-2

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
-// require("dotenv").config(path: './env');
+//  require("dotenv").config();
+
 
 import dotenv from "dotenv";
-import express from "express";
-import mongoose, { connect } from "mongoose";
-import { DB_NAME } from "./constants";
+// dotenv.config({ path: "./env" });
 import connectDB from "./db/index.js";
-
+import { app } from "./app.js";
 dotenv.config({
-    path: './env'
-});
+    path: './.env'
+})
 
 connectDB()
 .then(() => {
