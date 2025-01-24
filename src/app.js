76bec82989ff7a932cd780db2fb1f70c84f975c2
omjain4 <119519103+omjain4@users.noreply.github.com>
2025-01-24 16:59:30 +0530

    user-controller, and user route created

--- src/app.js
+++ src/app.js
@@ -15,4 +15,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static('public'));
 
+
+//routes import
+import userRouter from './routes/user.routes.js'
+app.use("/api/v1/users", userRouter)
+
 export {app}
