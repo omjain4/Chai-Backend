a6cb8b8c2904adefc09f0797dd274ff25572b5a6
omjain4 <119519103+omjain4@users.noreply.github.com>
2025-02-04 00:49:49 +0530

    add morgan for logging and update CORS origin configuration

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -1,281 +1,366 @@
-import mongoose, {isValidObjectId} from "mongoose"
-import {Playlist} from "../models/playlist.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { ApiError } from "../utils/ApiError.js";
+import mongoose, { isValidObjectId } from "mongoose";
+import { Like } from "../models/like.model.js";
+import { Video } from "../models/video.model.js";
+import { Comment } from "../models/comment.model.js";
+import { Tweet } from "../models/tweet.model.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
+// TODO: Review and Enhance all controllers
 
-const createPlaylist = asyncHandler(async (req, res) => {
-    const {name, description} = req.body
-
-    //TODO: create playlist
-    //create playlist object
-    
-    if(!name || !description) {
-        throw new ApiError(400,"Name and description are required")
-    }
-
-    const playlist = await Playlist.create({
-        name,
-        description,
-        owner:req.user._id
-    })
-
-    if(!playlist){
-        throw new ApiError(500,"Failed to create playlist")
-    }
-    return res
-    .status(201)
-    .json(
-        new ApiResponse(201,playlist,"Playlist created successfully")
-    )
-})
-
-const getUserPlaylists = asyncHandler(async (req, res) => {
-    const {userId} = req.params
-    //TODO: get user playlists
-    //get playlists by userId
-
-    if(!isValidObjectId(userId)){
-        throw new ApiError(400,"Invalid user id")
-    }
-
-    const playlists = await Playlist.find({owner:userId})
-    if(!playlists){
-        throw new ApiError(404,"No playlists found")
-    }
-    return res
-    .status(200)
-    .json(
-        new ApiResponse(200,playlists,"Playlists fetched successfully")
-    )
-})
-
-const getPlaylistById = asyncHandler(async (req, res) => {
-    const {playlistId} = req.params
-    //TODO: get playlist by id
-
-    //get playlist by id
-
-    if(!isValidObjectId(playlistId)){
-        throw new ApiError(400,"Invalid playlist id")
-    }
-
-    const playlist = await Playlist.findById(playlistId)
-    if(!playlist){
-        throw new ApiError(404,"No playlist found")
-    }
-
-    return res
-    .status(200)
-    .json(
-        new ApiResponse(200,playlist,"Playlist fetched successfully")
-    )
-})
-
-const addVideoToPlaylist = asyncHandler(async (req, res) => {
-    const {playlistId, videoId} = req.params
-
-    //get playlist 
-    //add video to the playlist
-
-    if(!isValidObjectId(playlistId)){
-        throw new ApiError(400,"Invalid playlistId")
-    }
-
-    if(!isValidObjectId(videoId)){
-        throw new ApiError(400,"Invalid videoId")
-    }
-
-    const playlist = await Playlist.findByIdAndUpdate(
-        playlistId,
-        {
-          $addToSet: {
-            videos: videoId,
-          },
-        },
-        {
-          new: true,
-        }
-      );
-    
-      if (!playlist)
-        throw new ApiError(500, "Error while adding video to playlist");
-    
-      return res
-        .status(200)
-        .json(
-          new ApiResponse(
-            200,
-            { isAdded: true },
-            "Video added to playlist successfully"
-          )
-        );
-})
-
-const removeVideoFromPlaylist = asyncHandler(async (req, res) => {
-    const {playlistId, videoId} = req.params
-    // TODO: remove video from playlist
-
-    //get playlist 
-    //check if playlist owner is the same as the user  
-    //remove video from playlist
-
-    if(!isValidObjectId(playlistId) && !isValidObjectId(videoId)){
-        throw new ApiError(400,"Invalid playlistId or videoId")
-    }
-
-    const playlist = await Playlist.findById(playlistId)
-
-    if(!playlist){
-        throw new ApiError(404,"No playlist found")
-    }
-
-    if(!playlist.videos.includes(videoId)){
-        throw new ApiError(404,"Video not found in playlist")
-    }
-
-    if(!playlist.owner.equals(req.user._id)){
-        throw new ApiError(403,"You are not allowed to remove video from this playlist")
-    }
-
-    playlist.videos.pull(videoId)
-    const checkSaved = await playlist.save()
-
-    if(!checkSaved){
-        throw new ApiError(500,"Failed to remove video from playlist")
-    }
-
-    return res
-    .status(200)
-    .json(
-        new ApiResponse(
-            200,
-            { isSuccess: true },
-            "Video removed from playlist successfully"
-        )
-    )
-})
-
-const deletePlaylist = asyncHandler(async (req, res) => {
-    const {playlistId} = req.params
-    // TODO: delete playlist
-    //get playlist 
-    //verify playlist owner
-    //delete playlist
-
-    if(!isValidObjectId(playlistId)){
-        throw new ApiError(400,"Invalid playlistId")
-    }
-
-    const playlist = await Playlist.findById(playlistId)
-
-    if(!playlist){
-        throw new ApiError(404,"No playlist found")
-    }
-    
-    if(!playlist.owner.equals(req.user._id)){
-        throw new ApiError(403,"You are not allowed to delete this playlist")
-    }
-
-    const deletedPlaylist = await Playlist.findByIdAndDelete(playlistId)
-
-    if(!deletedPlaylist){
-        throw new ApiError(500,"Failed to delete playlist")
-    }
-
-    return res
-    .status(200)
-    .json(
-        new ApiResponse(200,"Playlist deleted successfully")
-    )
-})
-
-const updatePlaylist = asyncHandler(async (req, res) => {
-    const {playlistId} = req.params
-    const {name, description} = req.body
-    //TODO: update playlist
-
-    //get playlist 
-    //verify ownership
-    //update playlist
-    //save changes
-
-    if(!isValidObjectId(playlistId)){
-        throw new ApiError(400,"Invalid playlistID")
-    }
-
-    if(!name && !description){
-        throw new ApiError(400,"Name and description are required")
-    }
-
-    const playlist = await Playlist.findById(playlistId)
-    
-    if(!playlist){
-        throw new ApiError(404,"No playlist found")
-    }
-
-    if(!playlist.owner.equals(req.user._id)){
-        throw new ApiError(403,"You are not allowed to update this playlist")
-    }
-
-    playlist.name = name
-    playlist.description = description
-
-    const checkUpdated = await playlist.save()
-
-    if(!checkUpdated){
-        throw new ApiError(500,"Failed to update playlist")
-    }
-
-    return res 
-    .status(200)
-    .json(
-        new ApiResponse(
-        200,
-        playlist,
-        "Playlist updated successfully"
-    ))
-})
-
-const getVideoSavePlaylists = asyncHandler(async (req, res) => {
-    const { videoId } = req.params;
-  
-    if (!isValidObjectId(videoId))
-      throw new ApiError(400, "Valid videoId required");
-  
-    const playlists = await Playlist.aggregate([
-      {
-        $match: {
-          owner: new mongoose.Types.ObjectId(req.user?._id),
-        },
+const getLikedVideos = asyncHandler(async (req, res) => {
+  const likedVideos = await Like.aggregate([
+    {
+      $match: {
+        video: { $ne: null },
+        likedBy: new mongoose.Types.ObjectId(req.user?._id),
       },
-      {
-        $project: {
-          name: 1,
-          isVideoPresent: {
-            $cond: {
-              if: { $in: [new mongoose.Types.ObjectId(videoId), "$videos"] },
-              then: true,
-              else: false,
+    },
+    {
+      $lookup: {
+        from: "videos",
+        localField: "video",
+        foreignField: "_id",
+        as: "video",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    username: 1,
+                    fullName: 1,
+                    avatar: 1,
+                  },
+                },
+              ],
             },
           },
-        },
+          {
+            $unwind: "$owner",
+          },
+        ],
       },
-    ]);
-  
-    return res
-      .status(200)
-      .json(new ApiResponse(200, playlists, "Playlists sent successfully"));
+    },
+    {
+      $unwind: "$video",
+    },
+    {
+      $match: {
+        "video.isPublished": true,
+      },
+    },
+    {
+      $group: {
+        _id: "likedBy",
+        videos: { $push: "$video" },
+      },
+    },
+  ]);
+
+  const videos = likedVideos[0]?.videos || [];
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, videos, "videos sent successfully"));
+});
+
+const toggleLike = asyncHandler(async (req, res) => {
+  const { toggleLike, commentId, videoId, tweetId } = req.query;
+
+  let reqLike;
+
+  if (
+    !isValidObjectId(commentId) &&
+    !isValidObjectId(tweetId) &&
+    !isValidObjectId(videoId)
+  )
+    throw new ApiError(400, "Invalid id");
+
+  if (toggleLike === "true") reqLike = true;
+  else if (toggleLike === "false") reqLike = false;
+  else throw new ApiError(400, "Invalid query string!!!");
+
+  let userLike;
+
+  if (commentId) {
+    const comment = await Comment.findById(commentId);
+    if (!comment) throw new ApiError(400, "No comment found");
+
+    userLike = await Like.find({
+      comment: commentId,
+      likedBy: req.user?._id,
+    });
+  } else if (videoId) {
+    const video = await Video.findById(videoId);
+    if (!video) throw new ApiError(400, "No video found");
+
+    userLike = await Like.find({
+      video: videoId,
+      likedBy: req.user?._id,
+    });
+  } else if (tweetId) {
+    const tweet = await Tweet.findById(tweetId);
+    if (!tweet) throw new ApiError(400, "No tweet found");
+
+    userLike = await Like.find({
+      tweet: tweetId,
+      likedBy: req.user?._id,
+    });
+  }
+
+  let isLiked = false;
+  let isDisLiked = false;
+
+  if (userLike?.length > 0) {
+    // entry is present so toggle status
+    if (userLike[0].liked) {
+      // like is present
+      if (reqLike) {
+        // toggle like so delete like
+        await Like.findByIdAndDelete(userLike[0]._id);
+        isLiked = false;
+        isDisLiked = false;
+      } else {
+        // toggle dis-like so make it dislike
+        userLike[0].liked = false;
+        let res = await userLike[0].save();
+        if (!res) throw new ApiError(500, "error while updating like");
+        isLiked = false;
+        isDisLiked = true;
+      }
+    } else {
+      // dis-like is present
+      if (reqLike) {
+        // toggle like so make it like
+        userLike[0].liked = true;
+        let res = await userLike[0].save();
+        if (!res) throw new ApiError(500, "error while updating like");
+        isLiked = true;
+        isDisLiked = false;
+      } else {
+        // toggle dis-like so delete dis-like
+        await Like.findByIdAndDelete(userLike[0]._id);
+        isLiked = false;
+        isDisLiked = false;
+      }
+    }
+  } else {
+    // entry is not present so create new
+    let like;
+    if (commentId) {
+      like = await Like.create({
+        comment: commentId,
+        likedBy: req.user?._id,
+        liked: reqLike,
+      });
+    } else if (videoId) {
+      like = await Like.create({
+        video: videoId,
+        likedBy: req.user?._id,
+        liked: reqLike,
+      });
+    } else if (tweetId) {
+      like = await Like.create({
+        tweet: tweetId,
+        likedBy: req.user?._id,
+        liked: reqLike,
+      });
+    }
+    if (!like) throw new ApiError(500, "error while toggling like");
+    isLiked = reqLike;
+    isDisLiked = !reqLike;
+  }
+
+  let totalDisLikes, totalLikes;
+
+  if (commentId) {
+    totalLikes = await Like.find({ comment: commentId, liked: true });
+    totalDisLikes = await Like.find({ comment: commentId, liked: false });
+  } else if (videoId) {
+    totalLikes = await Like.find({ video: videoId, liked: true });
+    totalDisLikes = await Like.find({ video: videoId, liked: false });
+  } else if (tweetId) {
+    totalLikes = await Like.find({ tweet: tweetId, liked: true });
+    totalDisLikes = await Like.find({ tweet: tweetId, liked: false });
+  }
+
+  return res.status(200).json(
+    new ApiResponse(
+      200,
+      {
+        isLiked,
+        totalLikes: totalLikes.length,
+        isDisLiked,
+        totalDisLikes: totalDisLikes.length,
+      },
+      "Like toggled successfully"
+    )
+  );
+});
+
+// Deprecated Controllers
+
+const toggleVideoLike = asyncHandler(async (req, res) => {
+  const { videoId } = req.params;
+  const { toggleLike } = req.query;
+
+  if (!isValidObjectId(videoId)) {
+    throw new ApiError(400, "invalid videoId");
+  }
+
+  const video = await Video.findById(videoId);
+  if (!video) throw new ApiError(400, "video not found");
+
+  let isLiked = await Like.find({ video: videoId, likedBy: req.user?._id });
+
+  if (isLiked && isLiked.length > 0) {
+    const like = await Like.findByIdAndDelete(isLiked[0]._id);
+    isLiked = false;
+  } else {
+    const like = await Like.create({ video: videoId, likedBy: req.user?._id });
+    if (!like) throw new ApiError(500, "error while toggling like");
+    isLiked = true;
+  }
+
+  let totalLikes = await Like.find({ video: videoId });
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        { isLiked, totalLikes: totalLikes.length },
+        "like toggled successfully"
+      )
+    );
+});
+
+const toggleCommentLike = asyncHandler(async (req, res) => {
+  const { commentId } = req.params;
+  const { toggleLike } = req.query;
+
+  let reqLike;
+
+  if (!isValidObjectId(commentId)) throw new ApiError(400, "invalid commentId");
+  if (toggleLike === "true") reqLike = true;
+  else if (toggleLike === "false") reqLike = false;
+  else throw new ApiError(400, "Invalid query string!!!");
+
+  const comment = await Comment.findById(commentId);
+  if (!comment) throw new ApiError(400, "no comment found");
+
+  let userLike = await Like.find({
+    comment: commentId,
+    likedBy: req.user?._id,
   });
 
+  let isLiked = false;
+  let isDisLiked = false;
+
+  if (userLike?.length > 0) {
+    // entry is present so toggle status
+    if (userLike[0].liked) {
+      // like is present
+      if (reqLike) {
+        // toggle like so delete like
+        await Like.findByIdAndDelete(userLike[0]._id);
+        isLiked = false;
+        isDisLiked = false;
+      } else {
+        // toggle dis-like so make it dislike
+        userLike[0].liked = false;
+        let res = await userLike[0].save();
+        if (!res) throw new ApiError(500, "error while updating like");
+        isLiked = false;
+        isDisLiked = true;
+      }
+    } else {
+      // dis-like is present
+      if (reqLike) {
+        // toggle like so make it like
+        userLike[0].liked = true;
+        let res = await userLike[0].save();
+        if (!res) throw new ApiError(500, "error while updating like");
+        isLiked = true;
+        isDisLiked = false;
+      } else {
+        // toggle dis-like so delete dis-like
+        await Like.findByIdAndDelete(userLike[0]._id);
+        isLiked = false;
+        isDisLiked = false;
+      }
+    }
+  } else {
+    // entry is not present so create new
+    const like = await Like.create({
+      comment: commentId,
+      likedBy: req.user?._id,
+      liked: reqLike,
+    });
+    if (!like) throw new ApiError(500, "error while toggling like");
+    isLiked = reqLike;
+    isDisLiked = !reqLike;
+  }
+
+  let totalLikes = await Like.find({ comment: commentId, liked: true });
+  let totalDisLikes = await Like.find({ comment: commentId, liked: false });
+
+  return res.status(200).json(
+    new ApiResponse(
+      200,
+      {
+        isLiked,
+        totalLikes: totalLikes.length,
+        isDisLiked,
+        totalDisLikes: totalDisLikes.length,
+      },
+      "Comment like toggled successfully"
+    )
+  );
+});
+
+const toggleTweetLike = asyncHandler(async (req, res) => {
+  const { tweetId } = req.params;
+  const { toggleLike } = req.query;
+
+  if (!isValidObjectId(tweetId)) throw new ApiError(400, "invalid tweetId");
+  const tweet = await Tweet.findById(tweetId);
+  if (!tweet) throw new ApiError(400, "no tweet found");
+
+  let isLiked = await Like.find({ tweet: tweetId, likedBy: req.user?._id });
+
+  if (isLiked?.length > 0) {
+    await Like.findByIdAndDelete(isLiked[0]._id);
+    isLiked = false;
+  } else {
+    const like = await Like.create({ tweet: tweetId, likedBy: req.user?._id });
+    if (!like) throw new ApiError(500, "error while toggling like");
+    isLiked = true;
+  }
+
+  let totalLikes = await Like.find({ tweet: tweetId });
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        { isLiked, totalLikes: totalLikes.length },
+        "Tweet like toggled successfully"
+      )
+    );
+});
+
 export {
-    createPlaylist,
-    getUserPlaylists,
-    getPlaylistById,
-    addVideoToPlaylist,
-    removeVideoFromPlaylist,
-    deletePlaylist,
-    updatePlaylist,
-    getVideoSavePlaylists
-}
+  toggleCommentLike,
+  toggleTweetLike,
+  toggleVideoLike,
+  getLikedVideos,
+  toggleLike,
+};
